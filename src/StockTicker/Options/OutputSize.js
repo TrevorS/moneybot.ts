dafeb61aa50c08ef8b3c50113be9aefaa41ddbd7
Trevor Strieber <trevor@strieber.org>
2017-08-20 07:14:30 -0700

    Remove accidental JS.
    * Add Slack library.
    * Add MoneyBot structure.

--- src/StockTicker/Options/OutputSize.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var OUTPUT_SIZE;
-(function (OUTPUT_SIZE) {
-    OUTPUT_SIZE["COMPACT"] = "compact";
-    OUTPUT_SIZE["FULL"] = "full";
-})(OUTPUT_SIZE || (OUTPUT_SIZE = {}));
-exports["default"] = OUTPUT_SIZE;
